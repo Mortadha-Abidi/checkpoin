450be8c7d1a0157aebafd0310b52dd897c68e923
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2022-08-05 18:04:48 +0200

    react

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './Components/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './navbar/Nav'
+function App() {
+  return (
+    <div>
+    <BasicExample/>
+    <Form/>
+    </div>
+  );
+}
+
+export default App;
